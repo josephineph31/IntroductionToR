43c64153a2d20f66506ab37b587c754ca631f5a8
Ngoc Pham <baongocyu@gmail.com>
2025-04-22 16:21:37 +0900

    Delete Exercise 2.1.R

--- Exercise 2.1.R
+++
@@ -1,2 +0,0 @@
-students_information <- data.frame (First_name = c('Taylor', 'Olivia', 'Katy'), Last_name = c('Swift', 'Rodringo', 'Perry'), Age = c( 36, 22, 41), Birthdate = c('12-13-1989','02-20-2003', '10-25-1984' ), Nationality = c('American', 'Fillipino', 'American'), Gender = c('Female', 'Female', 'Female'))
-View(students_information)
